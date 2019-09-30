034f37164b9670b908765f3e0477964f8e416aa6
Andres Gesteira <gesteira.pro@gmail.com>
2019-09-30 19:00:38 +0200

    start with onAuthStateChanged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'crf-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'craftnote-firebase';
+  constructor(private angularFireAuth: AngularFireAuth) {
+    this.angularFireAuth.auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        const displayName = user.displayName;
+        const email = user.email;
+        const emailVerified = user.emailVerified;
+        const photoURL = user.photoURL;
+        const isAnonymous = user.isAnonymous;
+        const uid = user.uid;
+        const providerData = user.providerData;
+        console.log('displayName:::', displayName);
+        console.log('email:::', email);
+        console.log('emailVerified:::', emailVerified);
+        console.log('photoURL:::', photoURL);
+        console.log('isAnonymous:::', isAnonymous);
+        console.log('uid:::', uid);
+        console.log('providerData:::', providerData);
+      } else {
+        // Navigate to registration.
+        console.log('We are not authenticated');
+      }
+    });
+  }
 }
