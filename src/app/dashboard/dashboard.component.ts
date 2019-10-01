ec8472d81aa3e379146dd7ab58e45e8ee6ba85e8
Andres Gesteira <gesteira.pro@gmail.com>
2019-10-01 14:09:16 +0200

    add a new feature to the collection

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, share } from 'rxjs/operators';
 import { Feature } from '../feature.model';
@@ -11,8 +12,16 @@ import { Feature } from '../feature.model';
 })
 export class DashboardComponent {
   features$: Observable<Feature[]>;
+  featureForm = this.fb.group({
+    name: ['', Validators.required],
+    importance: [null, Validators.required],
+    quantity: [null, Validators.required]
+  });
 
-  constructor(private angularFirestore: AngularFirestore) {
+  constructor(
+    private angularFirestore: AngularFirestore,
+    private fb: FormBuilder
+  ) {
     this.features$ = this.angularFirestore
       .collection<Feature>('Feature')
       .snapshotChanges()
@@ -31,5 +40,20 @@ export class DashboardComponent {
         ),
         share()
       );
+  }
+
+  onSubmit(): void {
+    this.angularFirestore
+      .collection<Feature>('Feature')
+      .add({
+        featureName: this.featureForm.value.name,
+        importance: this.featureForm.value.importance,
+        quantity: this.featureForm.value.quantity
+      })
+      .then(() => {
+        console.log('A  new feature was added to the database.');
+        this.featureForm.reset();
+      })
+      .catch(error => alert(error.message));
   }
 }
